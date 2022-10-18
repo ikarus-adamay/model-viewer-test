2c15c95a8d11287a6aef1e6ff10673fcffcee8e5
ikarus-adamay <admay.mann@ikarus3d.com>
2022-10-18 14:23:12 +0530

    create store

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import hamburger from '../assets/models/hamburger.glb'
 import "@google/model-viewer/dist/model-viewer";
 const Viewer = () => {
 
   return (
-    <model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="assets/models/hamburger.glb" shadow-intensity="1" camera-controls touch-action="pan-y"></model-viewer>
+    <model-viewer 
+        alt="Hamburger" 
+        src={hamburger} 
+        shadow-intensity="1" 
+        camera-controls 
+        touch-action="pan-y" 
+        style={{ width: "100vw", height: "90vh" }}>
+            <button>Play</button>
+    </model-viewer>
   )
 }
 
